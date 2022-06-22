a4736d79c633f647e6492b38574d167fe4a9b056
laurenkumar <lauren_d@outlook.fr>
2022-06-22 21:25:01 +0200

    ajout transak

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import "./Main.css";
 import { makeStyles } from '@material-ui/core/styles';
